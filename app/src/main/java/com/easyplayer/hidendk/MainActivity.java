6551f134ac0d5f3c4fb4b1c695d5a653490f80d4
Muhaiminur Rahman <muhaiminurabir@gmail.com>
2019-10-30 15:50:03 +0600

    FINAL

--- app/src/main/java/com/easyplayer/hidendk/MainActivity.java
+++ app/src/main/java/com/easyplayer/hidendk/MainActivity.java
@@ -1,14 +1,25 @@
 package com.easyplayer.hidendk;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +28,19 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    @BindView(R.id.add_card)
+    Button addCard;
+    @BindView(R.id.remove_cart)
+    Button removeCart;
+
+    static int mNotifCount = 0;
+    Button notifCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
@@ -44,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 
     /**
@@ -55,4 +77,65 @@ public class MainActivity extends AppCompatActivity {
     public native String apikey2();
 
     public native int apikey3(int i);
+
+    @OnClick({R.id.add_card, R.id.remove_cart})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.add_card:
+                mNotifCount++;
+                setNotifCount(mNotifCount);
+                break;
+            case R.id.remove_cart:
+                mNotifCount--;
+                setNotifCount(mNotifCount);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        View count = menu.findItem(R.id.badge).getActionView();
+        notifCount = count.findViewById(R.id.notif_count);
+        notifCount.setText(String.valueOf(mNotifCount));
+
+        MenuItem menuItem = menu.findItem(R.id.action_settings);
+        menuItem.setIcon(buildCounterDrawable(R.drawable.ic_shopping_cart));
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setNotifCount(int count){
+        mNotifCount = count;
+        invalidateOptionsMenu();
+    }
+
+    //create notification id
+    private Drawable buildCounterDrawable(int backgroundImageId) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.cart_menuitem_layout, null);
+        view.setBackgroundResource(backgroundImageId);
+        if (mNotifCount == 0) {
+            View counterTextPanel = view.findViewById(R.id.counterValuePanel);
+            counterTextPanel.setVisibility(View.GONE);
+        } else {
+            TextView textView = view.findViewById(R.id.count);
+            textView.setText("" + mNotifCount+"");
+        }
+
+        view.measure(
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+
+        view.setDrawingCacheEnabled(true);
+        view.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+        view.setDrawingCacheEnabled(false);
+
+        return new BitmapDrawable(getResources(), bitmap);
+    }
 }
